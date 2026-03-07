36732a522596ba4b74521f53fe51fac60fa28f78
vertigovarbende <199499929+vertigovarbende@users.noreply.github.com>
2026-03-08 00:14:17 +0300

    initial commit

---
+++ eureka-server/src/test/java/com/deveyk/ws/api/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveyk.ws.api.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
